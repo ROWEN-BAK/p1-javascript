45b856c669fb9a82a54ae450f01fd7eb7c089454
ROWEN-BAK <9018934@student.zadkine.nl>
2022-09-15 14:40:07 +0200

    new files

---
+++ oefenen/test.js
@@ -0,0 +1,9 @@
+let MyName = "Rowen";
+let MyAge = 15;
+const diploma = Boolean = true;
+let passed
+
+document.write(MyName);
+document.write(MyAge)
+document.write(diploma)
+document.write(passed)
