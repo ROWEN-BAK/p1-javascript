4a0198839ed603fb5103e45386abc3bf6685dd2d
ROWEN-BAK <9018934@student.zadkine.nl>
2022-09-15 14:58:31 +0200

    update

--- oefenen/oefenen P2/test.js
+++ oefenen/oefenen P2/test.js
@@ -3,11 +3,11 @@ let num2 = 0;
 let answer = num1 / num2;
 console.log(answer);
 
-let answer = num1 - num2;
+let answer2 = num1 - num2;
 console.log(answer2)
 
-let answer = num1 + num2;
+let answer3 = num1 + num2;
 console.log(answer3);
 
-let answer = num1 * num2;
+let answer4 = num1 * num2;
 console.log(answer4);
