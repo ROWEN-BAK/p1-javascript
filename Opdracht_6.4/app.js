97a0da2bd0576d784365957084c50c9e3cfa88f9
ROWEN-BAK <9018934@student.zadkine.nl>
2022-10-06 14:21:34 +0200

    Opdracht 6.4

---
+++ Opdracht_6.4/app.js
@@ -0,0 +1,20 @@
+function calculate(num1, num2) {
+    answer = num1 + num2;
+    console.log(answer);
+}
+
+calculate(1, 12);
+
+function multiplyByFive(num3) {
+    answer = num3 * 5;
+    console.log(answer);
+}
+
+multiplyByFive(2);
+
+function calculateMinutes(num4) {
+    answer = num4 / 60;
+    console.log(answer);
+}
+
+calculateMinutes(3600);
